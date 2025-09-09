78e3ef2ad513614df7ed55b49e5053025b86f025
Ester <esterdasilvabertolani@gmail.com>
2025-09-08 22:02:23 -0300

    Versão Final

--- Trab_LOO_Parte2/ArrayList_Numeros.java
+++ Trab_LOO_Parte2/ArrayList_Numeros.java
@@ -2,9 +2,9 @@ package Trab_LOO_Parte2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class ArrayList_Numeros {
     /*
@@ -17,24 +17,34 @@ public class ArrayList_Numeros {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        ArrayList<Integer> lista1 = new ArrayList<>(
-                Arrays.asList(5, 12, 3, 7, 5, 9, 1, 12, 8, 3, 15, 7, 20, 2, 9, 4, 6, 11, 2, 14));
-        ArrayList<Integer> lista2 = new ArrayList<>();
+        /*
+         * ArrayList<Integer> lista1 = new ArrayList<>(
+         * Arrays.asList(5, 12, 3, 7, 5, 9, 1, 12, 8, 3, 15, 7, 20, 2, 9, 4, 6, 11, 2,
+         * 14));
+         */
 
-        System.out.println(lista1);
+        ArrayList<Integer> lista1 = new ArrayList<>();
 
-        for (Integer num : lista1) {
-            if (!(lista1.contains(num))) { // se já tiver o número não adiciona na lista 2
-                lista2.add(num);
-            }
+        int i = 0;
+        while (i < 20) {
+            System.out.print("Digite o " + (i + 1) + "° número: ");
+            Integer num = scan.nextInt();
+            lista1.add(num);
+            i++;
         }
 
-        System.out.println(lista2);
+        System.out.println("Lista 1: " + lista1);
 
-        Collections.sort(lista2); // ordena lista 2
+        TreeSet<Integer> conjunto = new TreeSet<>(lista1);
+        ArrayList<Integer> lista2 = new ArrayList<>(conjunto);
 
-        System.out.println();
-        System.out.println(lista2);
+        System.out.println("Lista 2 " + lista2); // essa dlc ja vem ordenada e sem ta duplicada slc brabissima
+
+        int tamanho = lista2.size();
+        int metade = tamanho / 2;
+        Collections.reverse(lista2.subList(metade, tamanho));
+
+        System.out.println("Lista 2 depois que o pai aq inverteu a segunda metade: " + lista2);
 
         scan.close();
     }
